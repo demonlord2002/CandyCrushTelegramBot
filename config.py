4d1f9ecda5ae82b844b6ba0d257d83cb9e862736
demonlord2002 <drdoom2003p@gmail.com>
2026-01-21 18:46:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URL = os.getenv("MONGO_URL")
+
+BOARD_SIZE = 4
+COOLDOWN_SECONDS = 10
+
+# Candy + Fruits emojis
+CANDIES = [
+    "🍬", "🍭", "🍫", "🍪",
+    "🍓", "🍇", "🍉"
+]
