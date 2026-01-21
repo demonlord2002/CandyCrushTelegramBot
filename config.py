2b91eeb7ef2fd5e19d32ae8a437d30656dc949b2
demonlord2002 <drdoom2003p@gmail.com>
2026-01-21 19:55:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+# config.py
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 
-BOARD_SIZE = 8
+GRID_SIZE = 8
 COOLDOWN_SECONDS = 10
+POINTS_PER_CANDY = 10
 
-# Candy + Fruits emojis
 CANDIES = [
     "🍬", "🍭", "🍫", "🍪",
-    "🍓", "🍇", "🍉"
+    "🍓", "🍇", "🍉", "🍒", "🍋"
 ]
